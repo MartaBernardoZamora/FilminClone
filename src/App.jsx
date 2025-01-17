5fb5589cd5f84aaabc6e8db59036495ba87170a6
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-17 14:46:11 +0100

    save change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Example from './Examples'
+import Modal from "./components/main/modal/Modal"
 import './App.css'
 
 function App() {
   return (
     <>
-      <Example />
+      <Modal />
     </>
   )
 }
--- src/components/main/modal/Modal.jsx
+++ src/components/main/modal/Modal.jsx
@@ -1,3 +1,5 @@
+import './Modal.css'
+
 function Modal() {
     const product= {
         title: "Cadena perpetua",
@@ -8,12 +10,11 @@ function Modal() {
         genres: ["drama", "crimen"],
         overview: "Acusado del asesinato de su mujer, Andrew Dufresne, tras ser condenado a cadena perpetua, es enviado a la prisión de Shawshank. Con el paso de los años conseguirá ganarse la confianza del director del centro y el respeto de sus compañeros presidiarios, especialmente de Red, el jefe de la mafia de los sobornos."
     }
-    let nomRating=
+    const nomRating=
         product.rating < 5 ? "Poco recomendable" :
         product.rating >=5 && product.rating <7 ? "Buena":
         product.rating >=7 && product.rating <9 ? "Genial":
         "Excelente";
-
     return (
         <article className="modal">
             <video autoPlay muted>
@@ -21,21 +22,30 @@ function Modal() {
             </video>
             <div className="modalInfo">
                 <div className="modalInfoVotes">
-                    <p>{product.rating}</p>
-                    <p>{product.votes} votos</p>
-                    <p>{nomRating}</p>
+                    <div className="containerRating">
+                        <p className="modalTextBold">{product.rating}</p>
+                    </div>
+                    <div className="containerVotes">
+                        <p className="modalTextBold">{product.votes} votos</p>
+                        <p>{nomRating}</p>
+                    </div>
                 </div>
                 <button className="modalInfoButton">VER</button>
                 <div className="modalInfoHeader">
-                    <p>{product.title}</p>
-                    <p>{product.duration}</p>
-                    <p>{product.pegi}</p>
-                    {product.genres.map((genre,index) =>
-                        <p key={`genre${index}`}>{genre}</p>
-                    )}
-                    <p></p>
+                    <p className="modalTextBold">{product.title}</p>
+                    <div className="modalData">
+                        <p >{product.duration}</p>
+                        <p>{product.pegi}</p>
+                    </div>
+                    <div className="modalGenre">                        
+                        {product.genres.map((genre,index) =>
+                            <p key={index}>{genre.toLocaleUpperCase()}</p>
+                        )}
+                    </div>
                 </div>
-                <div className="modalInfoContent">{product.overview}</div>
+                <div className="modalInfoContent">
+                    <p>{product.overview}</p>
+                </div>
             </div>
         </article>
     )
